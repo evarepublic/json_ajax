5c45d9f76a11ced6d81d9bf02d024ff3393db4e4
Ever <ever4ever@yandex.ru>
2022-03-05 19:53:50 +0300

    Запрос переписан с использованием fetch

--- js/main.js
+++ js/main.js
@@ -5,23 +5,21 @@ document.addEventListener("DOMContentLoaded", () => {
     output = document.getElementById("output");
 
   select.addEventListener("change", () => {
-    const request = new XMLHttpRequest();
-    request.open("GET", ".././cars.json");
-    request.setRequestHeader("Content-Type", "application/json");
-    request.send();
-    request.addEventListener("readystatechange", () => {
-      if (request.readyState === 4 && request.status === 200) {
-        const data = JSON.parse(request.responseText);
+    fetch("./cars.json")
+      .then((res) => {
+        if (res.status !== 200) {
+          throw new Error("status not 200");
+        }
+        return res.json();
+      })
+      .then((data) => {
         data.cars.forEach((item) => {
           if (item.brand === select.value) {
             const { brand, model, price } = item;
-            output.innerHTML = `
-              Car ${brand} ${model} : <span> ${price}</span> $`;
+            output.innerHTML = `${brand} ${model} <br> ${price}$`;
           }
         });
-      } else {
-        output.innerHTML = `Error :(`;
-      }
-    });
+      })
+      .catch((err) => console.error(err));
   });
 });
